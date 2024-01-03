5d7820c9fb13b0202d7b6f4fb99c81cedd68291f
Sameer Rifat <sameerrifatbrw789@gmail.com>
2024-01-03 12:26:26 +0500

    remove bugs

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React from 'react'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Link from 'next/link';
 import { useMediaQuery } from 'react-responsive';
@@ -10,23 +10,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const SlideContent = ({ image }) => {
-    const [forceRerender, setForceRerender] = useState(false);
-
-    const isSmallScreen = useMediaQuery({ maxWidth: 580 });
-    // const isMediumScreen = useMediaQuery({ minWidth: 581, maxWidth: 1280 });
-
-    // useEffect(() => {
-    //     setForceRerender(prevState => !prevState);
-    // }, [isSmallScreen, isMediumScreen]);
-
 
     return (
         <div
-        // key={forceRerender}
         className='h-[30vh] xxxs:h-[32vh] xs:h-[40vh] sm:h-[50vh] lg:h-[80vh] w-full'
         >
             <Image
-                // src={isSmallScreen ? image2 : image}
                 src={image}
                 alt="banner"
                 width={1200}
@@ -35,64 +24,6 @@ const SlideContent = ({ image }) => {
                 priority={true}
                 className='max-w-full w-full h-full -z-50'
             />
-            {/* <div className='w-full h-full absolute top-0 left-0 flex items-center'>
-                <div className='shared_container flex flex-col h-fit '>
-                    <div className=' flex items-center gap-1 xs:gap-3'>
-                        <span className='hidden xxs:block w-8 xs:w-14 sm:w-16 h-[1px] sm:h-0.5 rounded-full bg-black'></span>
-                        <h3 className='hidden xxxs:block xxxs:text-[10px] xs:text-xs sm:text-sm xxs:font-medium xs:font-semibold uppercase'>
-                            Welcome to our <span className='font-medium xs:font-bold'>TheCleaners</span> cleaning services
-                        </h3>
-                    </div>
-                    <h1
-                        className='font-medium xxs:font-semibold xs:font-bold text-sm xxxs:text-lg xxs:text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl block xxxs:hidden my-3 lg:my-5'
-                    >
-                        The Reliable Solution to all your {" "}
-                        <span className='text-base xxxs:text-xl xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-[0px] xs:leading-none'>Residential</span>
-                        {" "} and {" "}
-                        <span className='text-base xxxs:text-xl xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-none'>Commercial</span>
-                        {" "} Cleaning Needs
-                    </h1>
-                    <h1
-                        className='font-medium xxxs:font-semibold xs:font-bold text-lg xxs:text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl hidden xxxs:flex xs:hidden flex-col gap-3 xxs:gap-1 leading-none my-5 xxs:my-3'
-                    >
-                        <span>
-                            The Reliable Solution to all your
-                        </span>
-                        <span>
-                            <span className='text-[21px] xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-[0px] xs:leading-none'>Residential</span>
-                            {" "} and {" "}
-                            <span className='text-[21px] xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-none'>Commercial</span>
-                        </span>
-                        <span>
-                            Cleaning Needs
-                        </span>
-                    </h1>
-                    <h1
-                        className='font-semibold xs:font-bold text-balance xxs:text-xl xxxs:text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl hidden xs:flex flex-col gap-0 xs:gap-0.5 sm:gap-2 leading-[0px] xs:leading-none my-1 xs:my-3 lg:my-5'
-                    >
-                        <span>
-                            The Reliable Solution
-                        </span>
-                        <span>
-                            to all your <span className='text-lg xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-[0px] xs:leading-none'>Residential</span>
-                        </span>
-                        <span>
-                            and <span className='text-lg xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-none'>Commercial</span>
-                        </span>
-                        <span>
-                            Cleaning Needs
-                        </span>
-                    </h1>
-                    <Link
-                        href="/services"
-                        className="group w-fit bg_gradient text-white font-medium text-sm xxxs:text-base px-2 xxxs:px-3 xs:px-4 sm:px-5 md:px-6 py-2 xxs:py-3 md:py-4 rounded-sm flex items-center justify-between gap-1 xs:gap-2
-                                        outline-none shadow-lg hover:scale-105 hover:shadow-xl focus:scale-110 hover_bg_gradient active:scale-105 transition-all"
-                    >
-                        <span className='hidden xxs:flex'>Explore</span> Our Services{" "}
-                        <ArrowForwardIcon className=" xxs:opacity-80 group-hover:translate-x-1 transition-all" fontSize={isSmallScreen ? 'small' : 'medium'} />
-                    </Link>
-                </div>
-            </div> */}
         </div>
     );
 };
@@ -144,7 +75,7 @@ const Hero = () => {
 
                 </div>
                 <div className='w-full h-full absolute top-0 left-0 flex items-center z-[9999]'>
-                    <div className='shared_container flex flex-col h-fit '>
+                    <div className='shared_container flex flex-col h-fit'>
                         <div className=' flex items-center gap-1 xs:gap-3'>
                             <span className='hidden xxs:block w-8 xs:w-14 sm:w-16 h-[1px] sm:h-0.5 rounded-full bg-black'></span>
                             <h3 className='hidden xxxs:block xxxs:text-[10px] xs:text-xs sm:text-sm xxs:font-medium xs:font-semibold uppercase'>
